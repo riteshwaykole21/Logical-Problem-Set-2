6dd36cfaed842b63389fa60c3c42c1aef7844704
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-22 22:17:41 +0530

    [Ritesh]add :Number Rows

---
+++ BasicProgram/NoSepration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class NoSepration
+    {
+        public void Number()
+        {
+            Console.WriteLine("Enter A Number");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{X} {X} {X} {X}");
+            Console.WriteLine($"{X}{X}{X}{X}");
+            Console.WriteLine($"{X} {X} {X} {X}");
+            Console.WriteLine($"{X}{X}{X}{X}");
+        }
+    }
+}
--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -10,7 +10,8 @@ namespace BasicProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Your Age");
+            Console.WriteLine("1 : Enter Your Age");
+            Console.WriteLine("2 : Enter A Number");
             Console.WriteLine("Enter A Option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -18,6 +19,10 @@ namespace BasicProgram
                 case 1:
                     Age age = new Age();
                     age.Number();
+                    break;
+                case 2:
+                    NoSepration noSepration = new NoSepration();
+                    noSepration.Number();   
                     break;
             }
             Console.ReadLine();
