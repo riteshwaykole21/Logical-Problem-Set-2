7371d1a903ff52d3d233cd92b5cb108463cc36e2
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-22 21:05:50 +0530

    [Ritesh]add : Age Problem

---
+++ BasicProgram/Age.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class Age
+    {
+        public void Number()
+        {
+            Console.WriteLine("Enter Your Age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"You Look Older Than {Age}");
+        }
+    }
+}
---
+++ BasicProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age");
+            Console.WriteLine("Enter A Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Age age = new Age();
+                    age.Number();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
